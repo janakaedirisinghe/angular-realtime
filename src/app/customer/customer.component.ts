7ec97105c0bbae796e6d2f46233d527b2e341a24
janaka531 <janakapradeepedirisinghe@gmail.com>
2019-05-06 20:29:58 +0530

    fix form styling

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService} from '../shared/customer.service';
 
 @Component({
   selector: 'app-customer',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private customerService: CustomerService) { }
+  submitted: boolean;
+  formControls = this.customerService.form.controls;
+
 
   ngOnInit() {
+  }
+  onSubmit() {
+    this.submitted = true;
+    if (this.customerService.form.valid) {
+      // if (this.customerService.form.get('$key').value == null)
+      // update
+      this.submitted = false;
+    }
+
   }
 
 }
---
+++ src/app/shared/customer.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {FormControl, FormGroup , Validators} from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor() { }
+    form  = new FormGroup({
+    $key: new FormControl(null),
+    fullName: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.email),
+    mobile: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    location: new FormControl('')
+
+  });
+}
